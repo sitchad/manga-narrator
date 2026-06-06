bd61ebd7fa453256d6a087bb97636fdc2de255e3
TonPseudoGitHub <ton-email@example.com>
2026-06-06 19:27:28 +0100

    Suppression des dossiers inutiles

--- models/db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-def get_db_connection():
-    return psycopg2.connect(
-        database="postgres",
-        user="postgres.liiyfrmmwqsbsjbnmrwj",
-        password="19902450aA@zZ#",
-        host="aws-0-eu-west-1.pooler.supabase.com",
-        port="6543"
-    )
