a5830ce915efb498362dc914112ca3f3e4fae74f
TonPseudoGitHub <ton-email@example.com>
2026-06-06 21:03:13 +0100

    Réparation de la définition de l'application app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
-# ...
+from controllers.logic import get_mangas
+
+# Cette ligne est OBLIGATOIRE, ne l'efface jamais
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     try:
         mangas = get_mangas()
         return render_template('index.html', mangas=mangas)
     except Exception as e:
-        return f"Erreur critique : {str(e)}" # Cela affichera l'erreur sur ton site au lieu de 500
+        return f"Erreur critique : {str(e)}"
+
+if __name__ == '__main__':
+    app.run()
