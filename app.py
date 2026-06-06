af400e2e03f4978ddca8c67ed4a78be05851087c
TonPseudoGitHub <ton-email@example.com>
2026-06-06 20:26:52 +0100

    Ajout de l'interface HTML

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask, send_file
-from controllers.logic import get_mangas, get_page_data
-from gtts import gTTS
-import io
+from flask import Flask, render_template
+from controllers.logic import get_mangas
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Récupère la liste depuis ton contrôleur
     mangas = get_mangas()
-    return f"Application en ligne. Mangas trouvés : {len(mangas)}"
-
-@app.route('/manga/<int:manga_id>/page/<int:num_page>')
-def page(manga_id, num_page):
-    data = get_page_data(manga_id, num_page)
-    return str(data)
+    # Envoie les données vers le HTML
+    return render_template('index.html', mangas=mangas)
 
 if __name__ == '__main__':
     app.run()
